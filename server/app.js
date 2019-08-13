e6374c8bd26cfe15b2885f4460229fb6b7891705
hank <>
2019-08-14 10:51:00 +1200

    drop views

--- server/app.js
+++ server/app.js
@@ -1,31 +1,30 @@
 const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
 const logger = require('morgan')
 
 const graphql = require('./graphql')
-const indexRouter = require('./routes/index')
 const { authMiddleware } = require('./lib/auth')
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
 
 // auth
 app.use(authMiddleware)
 
+// index /
+app.get('/', (_, res) => {
+  res.json({
+    message: 'welcome',
+    api: '/api',
+    graphql: '/graphql'
+  })
+})
+
 // enable /graphql
 graphql.applyMiddleware({ app })
-
-// handle other routes
-app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,12 +34,10 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  // res.locals.message = err.message
-  // res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  // res.status(err.status || 500)
-  // res.render('error')
   return res.status(err.status || 500).json({ error: err.message })
 })
 
