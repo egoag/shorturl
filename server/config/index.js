4c352adbd61576418befd505c370a5832c02bac2
hank <hank@hanks-MacBook-Pro.local>
2019-09-06 20:21:57 +1200

    added configuration from environ variables

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,25 @@
-const HOUR = 1000 * 60 * 60
-const DAY = 24 * HOUR
+const defaultConfig = require('./config.json')
 
-module.exports = {
-  Server: {
-    Secret: 'jwt!',
-    Expiration: 7 * DAY
+const loadEnv = (obj, prefix = 'CONFIG') => {
+  for (const [key, val] of Object.entries(obj)) {
+    if (typeof val === 'object') {
+      // Object or Array
+      if (val.constructor === Array) {
+        console.error('not support array, keep it as it is.')
+      } else {
+        obj[key] = loadEnv(val, `${prefix}_${key}`)
+      }
+    } else {
+      // Number or String
+      const ENV_NAME = `${prefix.toUpperCase()}_${key.toUpperCase()}`
+      if (ENV_NAME in process.env) {
+        obj[key] = process.env[ENV_NAME]
+      }
+    }
   }
+  return obj
 }
+
+const Config = loadEnv({ ...defaultConfig })
+
+module.exports = Config
