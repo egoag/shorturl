1ce848920d0d3317a050b4cc239c4f565495aed6
hank <hank@hanks-MacBook-Pro.local>
2019-09-14 21:32:53 +1200

    added jest dynamodb test

---
+++ jest-dynamodb-config.js
@@ -0,0 +1,40 @@
+const { DB: { TableName, UserIndex } } = require('./config')
+
+module.exports = {
+  tables: [
+    {
+      TableName,
+      KeySchema: [
+        { AttributeName: 'id', KeyType: 'HASH' },
+        { AttributeName: 'version', KeyType: 'RANGE' }
+      ],
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: UserIndex,
+          KeySchema: [
+            { AttributeName: 'ownerId', KeyType: 'HASH' },
+            { AttributeName: 'updatedAt', KeyType: 'RANGE' }
+          ],
+          Projection: {
+            ProjectionType: 'KEYS_ONLY'
+          },
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+          }
+        }
+      ],
+      AttributeDefinitions: [
+        { AttributeName: 'id', AttributeType: 'S' },
+        { AttributeName: 'version', AttributeType: 'S' },
+        { AttributeName: 'ownerId', AttributeType: 'S' },
+        { AttributeName: 'updatedAt', AttributeType: 'S' }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+      }
+    }
+  ],
+  port: 8000
+}
